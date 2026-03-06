a0690aef3d08c7915b761cf18de76370ef1cb2bd
rovigo6894 <rovigo6894@gmail.com>
2026-03-06 13:41:21 -0600

    Update config_demo.py

--- config_demo.py
+++ config_demo.py
@@ -11,9 +11,6 @@ EMAIL = "contacto@optipension73.com"
 WHATSAPP = "https://wa.me/528715791810"
 WHATSAPP_NUMERO = "871 579 1810"
 
-# Redes sociales (opcional)
-SITIO_WEB = "https://optipension73.com"
-
 # Valores fijos para demo
 SEMANAS_FIJAS = 1315
 EDAD_RETIRO_FIJA = 60
