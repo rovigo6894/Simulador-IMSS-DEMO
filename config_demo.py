0bbaf2d36cdfac91561692698125febd8d53590f
rovigo6894 <rovigo6894@gmail.com>
2026-03-06 12:32:15 -0600

    Update config_demo.py

--- config_demo.py
+++ config_demo.py
@@ -1,1 +1,11 @@
-config_demo.py
+VERSION = "DEMO"
+
+PRO_FEATURES = {
+    "modalidad40": False,
+    "roi_inversion": False,
+    "simulacion_avanzada": False,
+    "exportar_pdf": False,
+    "calculo_real": False
+}
+
+WHATSAPP_LINK = "https://wa.me/528715791810?text=Hola%20quiero%20Optipension%2073%20PRO"
